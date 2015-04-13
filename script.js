e4d4dd35e0eb36e7baaebae7f6dd9c38c7c65834
Inspire Shahin <shahin.fci@gmail.com>
2015-04-13 14:06:28 +0600

    Sharing Scripts
    Script for sharing in facebook, google, twitter .

---
+++ script.js
@@ -0,0 +1,13 @@
+ <script>
+       <!--
+       try{
+       loc=location.pathname;
+       txt='<a href="http://www.facebook.com/sharer.php?u=http://www.belogin.com'+loc+'" target="_blank" title="Facebook"><span class="share sharefacebook social social-facebook"></span></a>';
+       txt=txt+'<a href="http://twitter.com/home?status=Currently reading http://www.belogin..com'+loc+'" target="_blank" title="Twitter"><span class="share sharetwitter social social-twitter"></span></a>';
+      txt=txt+'<a href="mailto:?&amp;subject='+document.title+'&amp;body=Take%20a%20look%20at%20this%20page%20at%20belogin..com:%20http://www.belogin..com'+loc+'" target="_blank"  title="E-mail"><span class="share shareemail glyphicons glyphicons-envelope"></span></a>';
+       txt=txt+'<a href="https://plus.google.com/share?url=http://www.belogin. com'+loc+'" target="_blank" title="Google+"><span class="share sharegoogle social social-google-plus"></span></a>';
+       document.write(txt);
+       }
+       catch(e) {}
+       //-->
+       </script>
